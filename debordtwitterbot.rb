d4996c7e82b3c40e63fafae6e350a66308b5d26a
Tim Waters <chippy2005@gmail.com>
2013-10-10 16:05:28 +0100

    first commit

---
+++ debordtwitterbot.rb
@@ -0,0 +1,21 @@
+require 'yaml'
+require 'twitter'
+
+config = YAML.load(open("config.yml"))
+Twitter.configure do |c|
+  c.consumer_key =  ENV["CONSUMER_KEY"] || config["CONSUMER_KEY"]
+  c.consumer_secret = ENV["CONSUMER_SECRET"] || config["CONSUMER_SECRET"]
+  c.oauth_token =  ENV["OAUTH_TOKEN"] || config["OAUTH_TOKEN"]
+  c.oauth_token_secret =  ENV["OAUTH_TOKEN_SECRET"] || config["OAUTH_TOKEN_SECRET"]
+end
+
+
+client = Twitter::Client.new
+
+puts "client starting"
+
+Twitter.search("psychogeography", :count => 3, :result_type => "recent").results.map do |status|
+  puts "#{status.from_user}: #{status.text}"
+end
+
+#client.update("But the irreversible time of the bourgeois economy eradicates these vestiges on every corner of the globe.")
