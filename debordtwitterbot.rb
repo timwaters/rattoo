2f89fc882945ca89c0267a17cb326557ac04224a
Tim Waters <chippy2005@gmail.com>
2013-10-12 14:21:07 +0100

    add following logic

--- debordtwitterbot.rb
+++ debordtwitterbot.rb
@@ -14,15 +14,41 @@ Twitter.configure do |c|
   c.oauth_token_secret =  ENV["OAUTH_TOKEN_SECRET"] || config["OAUTH_TOKEN_SECRET"]
 end
 
+
+#
+# markov chain the text
+#
 markov = MarkyMarkov::TemporaryDictionary.new
 markov.parse_file "debord.txt"
-
 raw_text = markov.generate_25_words
-
 tweet =  raw_text[0..132]
+
 
 client = Twitter::Client.new
 
+#
+# send the tweet
+#
+
 puts "sending text: " + tweet
 client.update(tweet)
+
+#
+# Now follow some people
+#
+
+to_follow = []
+client.search("psychogeography", :count => 3, :result_type => "recent").results.map do |status|
+ to_follow << status.from_user
+end
+client.search("guy debord", :count => 3, :result_type => "recent").results.map do |status|
+ to_follow << status.from_user
+end
+
+to_follow.uniq!
+
+to_follow.each do | new_user |
+  puts "following: " + new_user
+  client.follow(new_user)
+end unless to_follow.empty?
 
