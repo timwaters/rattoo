1a1302ab0fa838d5ceae985ce70298ea16724d0f
Tim Waters <chippy2005@gmail.com>
2013-12-03 14:39:14 +0000

    reply to people

--- debordtwitterbot.rb
+++ debordtwitterbot.rb
@@ -15,6 +15,20 @@ def truncate(text, length = 30)
 end
 
 
+#
+# markov chain the text
+#
+@markov = MarkyMarkov::TemporaryDictionary.new
+@markov.parse_file "debord.txt"
+
+def create_tweet
+  raw_text = @markov.generate_23_words
+  tweet_length = (110..135).to_a.sample
+  tweet = truncate(raw_text, tweet_length)
+
+  tweet
+end
+
 config = nil
 if File.exist?("config.yml")
   config = YAML.load(open("config.yml"))
@@ -26,41 +40,50 @@ Twitter.configure do |c|
   c.oauth_token_secret =  ENV["OAUTH_TOKEN_SECRET"] || config["OAUTH_TOKEN_SECRET"]
 end
 
-
-#
-# markov chain the text
-#
-markov = MarkyMarkov::TemporaryDictionary.new
-markov.parse_file "debord.txt"
-raw_text = markov.generate_23_words
-tweet_length = (110..135).to_a.sample
-tweet = truncate(raw_text, tweet_length)
-
 client = Twitter::Client.new
 
-#
-# send the tweet
-#
+old_tweet_id = client.user_timeline(client.user, {:count => 1, :exclude_replies => true})[0].id
 
+#
+# create and send the new tweet
+#
+tweet = create_tweet
 puts "sending text: " + tweet
-client.update(tweet)
+new_tweet = client.update(tweet)
+
+#
+# replies to last 3 questions
+#
+mentions = client.mentions({ :since_id => old_tweet_id, :count => 3})
+
+mentions.each do | m |
+
+if m.in_reply_to_user_id == client.user.id
+  reply  = "@#{m.user.screen_name} "+create_tweet()
+  puts "sending reply "+ reply
+  
+  client.update(reply, {:in_reply_to_status => m})
+end
+
+end
 
 #
 # Now follow some people
 #
+if false
+  to_follow = []
+  client.search("psychogeography", :count => 3, :result_type => "recent").results.map do |status|
+   to_follow << status.from_user
+  end
+  client.search("guy debord", :count => 3, :result_type => "recent").results.map do |status|
+   to_follow << status.from_user
+  end
 
-to_follow = []
-client.search("psychogeography", :count => 3, :result_type => "recent").results.map do |status|
- to_follow << status.from_user
+  to_follow.uniq!
+
+  to_follow.each do | new_user |
+    puts "following: " + new_user
+    #client.follow(new_user)
+  end unless to_follow.empty?
+  
 end
-client.search("guy debord", :count => 3, :result_type => "recent").results.map do |status|
- to_follow << status.from_user
-end
-
-to_follow.uniq!
-
-to_follow.each do | new_user |
-  puts "following: " + new_user
-  client.follow(new_user)
-end unless to_follow.empty?
-
